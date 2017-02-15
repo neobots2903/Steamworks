24bbf3c15224cf092b41782dad8d889b226e0b0c
John Burnham <buddybu@yahoo.com>
2017-02-14 17:29:31 -0800

    LIDAR Subsystem updates

--- src/org/usfirst/frc/team2903/robot/subsystems/LIDAR2903.java
+++ src/org/usfirst/frc/team2903/robot/subsystems/LIDAR2903.java
@@ -14,7 +14,14 @@ package org.usfirst.frc.team2903.robot.subsystems;
 		
 		private final int LIDAR_ADDR = 0x62;
 		private final int LIDAR_CONFIG_REGISTER = 0x00;
+		private final int SIG_COUNT_VAL_REGISTER = 0x02;
+		private final int ACQ_CONFIG_REGISTER = 0x04;
+		private final int THRESHOLD_BYPASS_REGISTER = 0x1c;
 		private final int LIDAR_DISTANCE_REGISTER = 0x8f;
+		
+		private final int RESET_FPGA_CMD = 0x00;
+		private final int DISTANCE_NOBIAS_CMD = 0x03;
+		private final int DISTANCE_BIAS_CMD = 0x04;
 		
 		public LIDAR2903(Port port) {
 			i2c = new I2C(port, LIDAR_ADDR);
@@ -22,11 +29,26 @@ package org.usfirst.frc.team2903.robot.subsystems;
 			distance = new byte[2];
 			
 			updater = new java.util.Timer();
+			
+			// reset
+			i2c.write(LIDAR_CONFIG_REGISTER, RESET_FPGA_CMD);
+			
+			// configure balanced performance
+			i2c.write(SIG_COUNT_VAL_REGISTER, 0x80);
+			i2c.write(ACQ_CONFIG_REGISTER, 0x08);
+			i2c.write(THRESHOLD_BYPASS_REGISTER, 0x00);
+			
 		}
 		
 		// Distance in cm
 		public int getDistance() {
-			return (int)Integer.toUnsignedLong(distance[0] << 8) + Byte.toUnsignedInt(distance[1]);
+			int distanceVal= 0;
+			for (int i = 0; i < 2; i++) {
+				int shift = (2 - 1 -i ) * 8;
+				distanceVal += (distance[i] & 0x000000FF) << shift;
+			}
+//			return (int)Integer.toUnsignedLong(distance[0] << 8) + Byte.toUnsignedInt(distance[1]);
+			return distanceVal;
 		}
 
 		public double pidGet() {
@@ -50,7 +72,7 @@ package org.usfirst.frc.team2903.robot.subsystems;
 		
 		// Update distance variable
 		public void update() {
-			i2c.write(LIDAR_CONFIG_REGISTER, 0x04); // Initiate measurement
+			i2c.write(LIDAR_CONFIG_REGISTER, DISTANCE_BIAS_CMD); // Initiate measurement
 			Timer.delay(0.04); // Delay for measurement to be taken
 			i2c.read(LIDAR_DISTANCE_REGISTER, 2, distance); // Read in measurement
 			Timer.delay(0.005); // Delay to prevent over polling
