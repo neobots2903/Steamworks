6f8d4ce78480d47d8429770d550ca0ea043e33ec
neobots <neobots2903@gmail.com>
2017-02-21 19:22:16 -0800

    Now it can drive straight for a few seconds and drop a gear while blind! So impressive :)

--- src/org/usfirst/frc/team2903/robot/commands/groups/StraightGearNoVision.java
+++ src/org/usfirst/frc/team2903/robot/commands/groups/StraightGearNoVision.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team2903.robot.commands.groups;
 import org.usfirst.frc.team2903.robot.Robot;
 import org.usfirst.frc.team2903.robot.commands.commoners.CloseArms;
 import org.usfirst.frc.team2903.robot.commands.commoners.DriveForTime;
+import org.usfirst.frc.team2903.robot.commands.commoners.DriveStraightWithGyro;
 import org.usfirst.frc.team2903.robot.commands.commoners.OpenArms;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -12,12 +13,18 @@ public class StraightGearNoVision extends CommandGroup {
 	public StraightGearNoVision() {
 		requires(Robot.driveSubsystem);
 		requires(Robot.gyroSubsystem);
-
+		
 		addSequential(new DriveForTime(4));
+		addParallel(new DriveStraightWithGyro());
 		addSequential(new OpenArms());
 //		addSequential(new DriveStraightForDistance(-1));
 		addSequential(new CloseArms());
 		
+		
+	}
+
+	private void addParallel(DriveStraightWithGyro driveStraightWithGyro) {
+		// TODO Auto-generated method stub
 		
 	}
 
