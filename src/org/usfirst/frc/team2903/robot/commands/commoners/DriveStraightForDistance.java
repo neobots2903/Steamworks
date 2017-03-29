4571d87d95fb6f3e23b1079dce9741bf90907e24
Burnham <listbuddy@frontier.com>
2017-03-28 21:07:42 -0700

    comments for Andrew

--- src/org/usfirst/frc/team2903/robot/commands/commoners/DriveStraightForDistance.java
+++ src/org/usfirst/frc/team2903/robot/commands/commoners/DriveStraightForDistance.java
@@ -1,3 +1,13 @@
+/*
+// ANDREW
+// ANDREW
+ * 
+ * We can remove all left side encoder stuff.  Other than that, the COUNTS_PER_MOTOR_REVOLUTION 
+ * looks correct. 
+ * 
+ * We can remove all of the Centimeter related stuff as well. 
+*/
+
 package org.usfirst.frc.team2903.robot.commands.commoners;
 
 import org.opencv.core.Rect;
@@ -26,7 +36,7 @@ public class DriveStraightForDistance extends Command {
 
 	int TargetEncoderPos = 0;
 	int CurrentRightEncoderPos = 0;
-	int CurrentLeftEncoderPos = 0;
+	int CurrentLeftEncoderPos = 0; // ANDREW
 	private int Distance = 0;
 
 	boolean ourJobIsDoneHere = false;
@@ -47,16 +57,16 @@ public class DriveStraightForDistance extends Command {
 														// circumference
 	static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * PI);
 
-	static final double CM_PER_INCH = 2.54;
-	static final double COUNTS_PER_CM = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION)
-			/ ((WHEEL_DIAMETER_INCHES * CM_PER_INCH) * PI);
+	static final double CM_PER_INCH = 2.54; // ANDREW
+	static final double COUNTS_PER_CM = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) 
+			/ ((WHEEL_DIAMETER_INCHES * CM_PER_INCH) * PI); // ANDREW
 
-	static final double TARGET_SPEED = 144; // revolutions per minute
-	static final double MIN_PER_SEC = 0.0167; // minute to second ratio
-	static final double SEC_PER_TVE = 0.1; // Seconds per 10 velocity
+	static final double TARGET_SPEED = 144; // revolutions per minute   // ANDREW
+	static final double MIN_PER_SEC = 0.0167; // minute to second ratio // ANDREW
+	static final double SEC_PER_TVE = 0.1; // Seconds per 10 velocity // ANDREW
 											// measurement periods (100ms)
-	static final double NATIVE_UNITS_PER_TVE = TARGET_SPEED * MIN_PER_SEC * SEC_PER_TVE * COUNTS_PER_MOTOR_REV;
-	static final double FULL_FORWARD = 1023;
+	static final double NATIVE_UNITS_PER_TVE = TARGET_SPEED * MIN_PER_SEC * SEC_PER_TVE * COUNTS_PER_MOTOR_REV; // ANDREW
+	static final double FULL_FORWARD = 1023;  // ANDREW
 
 	private Object imgLock = new Object();
 	private VisionThread visionThread;
@@ -104,7 +114,7 @@ public class DriveStraightForDistance extends Command {
 
 		// get current encoder counts
 		CurrentRightEncoderPos = Robot.driveSubsystem.rightGetRawCount();
-		CurrentLeftEncoderPos = Robot.driveSubsystem.leftGetRawCount();
+		CurrentLeftEncoderPos = Robot.driveSubsystem.leftGetRawCount(); // ANDREW
 
 		StartAngle = Robot.gyroSubsystem.GyroPosition();
 
@@ -112,10 +122,10 @@ public class DriveStraightForDistance extends Command {
 		TargetEncoderPos = (Distance * (int) COUNTS_PER_INCH);// +
 																// Robot.driveSubsystem.rightGetRawCount();
 
-		// SmartDashboard.putNumber("Right ", CurrentRightEncoderPos);
-		// SmartDashboard.putNumber("Left ", CurrentLeftEncoderPos);
-		// SmartDashboard.putNumber("DF distance", Distance);
-		// SmartDashboard.putNumber("DF Target", TargetEncoderPos);
+		// SmartDashboard.putNumber("Right ", CurrentRightEncoderPos); // ANDREW
+		// SmartDashboard.putNumber("Left ", CurrentLeftEncoderPos); // ANDREW
+		// SmartDashboard.putNumber("DF distance", Distance); // ANDREW
+		// SmartDashboard.putNumber("DF Target", TargetEncoderPos); // ANDREW
 
 		// setup the PID system
 		Robot.minipidSubsystem.reset();
@@ -126,9 +136,9 @@ public class DriveStraightForDistance extends Command {
 		HighLimit = TargetEncoderPos + ErrorLimit;
 		LowLimit = TargetEncoderPos - ErrorLimit;
 
-		// Robot.minipidSubsystem.setP(Kp);
-		// Robot.minipidSubsystem.setI(0);
-		// Robot.minipidSubsystem.setD(0);
+		// Robot.minipidSubsystem.setP(Kp); // ANDREW
+		// Robot.minipidSubsystem.setI(0); // ANDREW
+		// Robot.minipidSubsystem.setD(0); // ANDREW
 
 		ourJobIsDoneHere = false;
 	}
@@ -153,7 +163,7 @@ public class DriveStraightForDistance extends Command {
 	protected void execute() {
 
 		CurrentRightEncoderPos = Robot.driveSubsystem.rightGetRawCount();
-		CurrentLeftEncoderPos = Robot.driveSubsystem.leftGetRawCount();
+		CurrentLeftEncoderPos = Robot.driveSubsystem.leftGetRawCount(); // ANDREW
 		double angle;
 		MotorSpeed = Robot.minipidSubsystem.getOutput(CurrentRightEncoderPos, TargetEncoderPos) / 100;
 		if (UseGyro) {
@@ -183,7 +193,7 @@ public class DriveStraightForDistance extends Command {
 			MotorSpeed = -MinMotorSpeed;
 
 		SmartDashboard.putNumber("Right ", CurrentRightEncoderPos);
-		SmartDashboard.putNumber("Left ", CurrentLeftEncoderPos);
+		SmartDashboard.putNumber("Left ", CurrentLeftEncoderPos); // ANDREW
 		SmartDashboard.putNumber("Angle", angle);
 
 		// TODO Auto-generated method stub
@@ -197,39 +207,39 @@ public class DriveStraightForDistance extends Command {
 
 		// TODO Auto-generated method stub
 		CurrentRightEncoderPos = Robot.driveSubsystem.rightGetRawCount();
-		CurrentLeftEncoderPos = Robot.driveSubsystem.leftGetRawCount();
+		CurrentLeftEncoderPos = Robot.driveSubsystem.leftGetRawCount(); // ANDREW
 
 		SmartDashboard.putNumber("current right position", CurrentRightEncoderPos);
-		SmartDashboard.putNumber("current left position", CurrentLeftEncoderPos);
+		SmartDashboard.putNumber("current left position", CurrentLeftEncoderPos); // ANDREW
 		SmartDashboard.putNumber("target position", TargetEncoderPos);
 		SmartDashboard.putNumber("MOTOR SPEED", MotorSpeed);
 
-		// see if we have seen this saved encoder count before
-		if (CurrentRightEncoderPos != savedEncoderPosition) {
-			// new encoder count, save it off
-			savedEncoderPosition = CurrentRightEncoderPos;
-
-			// reset the counter
-			sameEncoderCount = 0;
-		} else {
-			// same encoder count -- register count
-			sameEncoderCount++;
-
-			// check to see if we have registered the same encoder count
-			// mulitple times in a row
-			// if (sameEncoderCount > MAX_SAME_ENCODER_COUNT) {
-			// /*
-			// * We could adjust the motor speed for each time we see the same
-			// * encoder count max times, but only if the motor speed is not 0
-			// */
-			// if (MotorSpeed != 0) {
-			// // increase minimum motor speed by 5% and reset the same count
-			// MinMotorSpeed += 0.05;
-			// sameEncoderCount = 0;
-			// } else {
-			// ourJobIsDoneHere = true;
-			// }
-			// }
+		// see if we have seen this saved encoder count before  // ANDREW
+		if (CurrentRightEncoderPos != savedEncoderPosition) { // ANDREW
+			// new encoder count, save it off // ANDREW
+			savedEncoderPosition = CurrentRightEncoderPos; // ANDREW
+			 // ANDREW
+			// reset the counter // ANDREW
+			sameEncoderCount = 0; // ANDREW
+		} else { // ANDREW
+			// same encoder count -- register count // ANDREW
+			sameEncoderCount++; // ANDREW
+			 // ANDREW
+			// check to see if we have registered the same encoder count // ANDREW
+			// mulitple times in a row // ANDREW
+			// if (sameEncoderCount > MAX_SAME_ENCODER_COUNT) { // ANDREW
+			// /* // ANDREW
+			// * We could adjust the motor speed for each time we see the same // ANDREW
+			// * encoder count max times, but only if the motor speed is not 0 // ANDREW
+			// */ // ANDREW
+			// if (MotorSpeed != 0) { // ANDREW
+			// // increase minimum motor speed by 5% and reset the same count // ANDREW
+			// MinMotorSpeed += 0.05; // ANDREW
+			// sameEncoderCount = 0; // ANDREW
+			// } else { // ANDREW
+			// ourJobIsDoneHere = true; // ANDREW
+			// } // ANDREW
+			// } // ANDREW
 		}
 
 		if (!ourJobIsDoneHere) {
@@ -254,10 +264,10 @@ public class DriveStraightForDistance extends Command {
 	@Override
 	protected void end() {
 		Robot.driveSubsystem.arcadeDrive(0, 0);
-		// CurrentRightEncoderPos = 0;
-		// TargetEncoderPos = 0;
-		// TODO Auto-generated method stub
-		// putting drives back into teleop mode
+		// CurrentRightEncoderPos = 0; // ANDREW
+		// TargetEncoderPos = 0; // ANDREW
+		// TODO Auto-generated method stub // ANDREW
+		// putting drives back into teleop mode // ANDREW
 		Robot.driveSubsystem.setTeleopMode();
 		ourJobIsDoneHere = false;
 	}
--- src/org/usfirst/frc/team2903/robot/subsystems/Drive2903.java
+++ src/org/usfirst/frc/team2903/robot/subsystems/Drive2903.java
@@ -1,3 +1,18 @@
+/*
+// ANDREW-TODO
+ * 
+ * Remove all lines marked with // ANDREW some might have ANDREW-TODO (as above)
+ * We can remove all left side encoder stuff.  Other than that, the COUNTS_PER_MOTOR_REVOLUTION 
+ * looks correct. 
+ * 
+ * We can remove all of the Centimeter and position stuff
+ * 
+ * we probably need to set the encoder count as well in drive reset.  its interesting that we put it there instead of in the initializer as we did on Bertha
+ * 
+ * I also noticed that bernard is still creating the camera instance in the drivestraight.  Maybe we should still be doing that since we only need
+ * the camera for driving and they can refresh the browser when teleop starts.  Maybe even have bernard set the camera to full brightness before exiting
+ * 
+*/
 package org.usfirst.frc.team2903.robot.subsystems;
 
 import org.usfirst.frc.team2903.robot.Robot;
@@ -20,35 +35,37 @@ public class Drive2903 extends Subsystem {
 	CANTalon rightFrontMotor;
 	CANTalon rightRearMotor;
 
-	static final double		PI						= 3.14159;
-    static final int     COUNTS_PER_MOTOR_REV    = 360 ;    // eg: Grayhill 61R256
-    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            												(WHEEL_DIAMETER_INCHES * PI);
+	static final double		PI						= 3.14159; // ANDREW
+	
+	// ANDREW-TODO -- this should be set to 256 and the line in DriveReset for the right encoder should be uncommented.
+    static final int     COUNTS_PER_MOTOR_REV    = 360 ;    // eg: Grayhill 61R256 
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP // ANDREW
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference // ANDREW
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / // ANDREW
+            												(WHEEL_DIAMETER_INCHES * PI); // ANDREW
     
-    static final double 	CM_PER_INCH             = 2.54;
-    static final double 	COUNTS_PER_CM           = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-    													((WHEEL_DIAMETER_INCHES * CM_PER_INCH) * PI);
+    static final double 	CM_PER_INCH             = 2.54; // ANDREW
+    static final double 	COUNTS_PER_CM           = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / // ANDREW
+    													((WHEEL_DIAMETER_INCHES * CM_PER_INCH) * PI); // ANDREW
     
-    static final double 	TARGET_SPEED			= 144;		// revolutions per minute
-    static final double 	MIN_PER_SEC				= 0.0167;  // minute to second ratio
-    static final double		SEC_PER_TVE				= 0.1;	   // Seconds per 10 velocity measurement periods (100ms)
-    static final double 	NATIVE_UNITS_PER_TVE	= TARGET_SPEED * MIN_PER_SEC * SEC_PER_TVE * COUNTS_PER_MOTOR_REV;
-    static final double		FULL_FORWARD			= 1023;
+    static final double 	TARGET_SPEED			= 144;		// revolutions per minute // ANDREW
+    static final double 	MIN_PER_SEC				= 0.0167;  // minute to second ratio // ANDREW
+    static final double		SEC_PER_TVE				= 0.1;	   // Seconds per 10 velocity measurement periods (100ms) // ANDREW
+    static final double 	NATIVE_UNITS_PER_TVE	= TARGET_SPEED * MIN_PER_SEC * SEC_PER_TVE * COUNTS_PER_MOTOR_REV; // ANDREW
+    static final double		FULL_FORWARD			= 1023; // ANDREW
     
-    // TARGET_SPEED needs to be determined from the maximum speed from the self-test display
-    // while running in teleop.  We probably only want to use 80% of that speed to make sure 
-    // that we can achieve the value
+    // TARGET_SPEED needs to be determined from the maximum speed from the self-test display // ANDREW
+    // while running in teleop.  We probably only want to use 80% of that speed to make sure  // ANDREW
+    // that we can achieve the value // ANDREW
     
-    // F-GAIN = FULL FORWARD / NATIVE_UNITS_PER_TVE
+    // F-GAIN = FULL FORWARD / NATIVE_UNITS_PER_TVE // ANDREW
     
-    // FULL FORWARD comes from the SELF-TEST for Motor Controller 1 or 3
+    // FULL FORWARD comes from the SELF-TEST for Motor Controller 1 or 3 // ANDREW
 
 	public RobotDrive robotDrive;
 	private int lastRightRawCount;
-	private int lastLeftRawCount;
-	private boolean autoPositionBothSides;
+	private int lastLeftRawCount; // ANDREW
+	private boolean autoPositionBothSides; // ANDREW
 	
 	public Drive2903() {
 
@@ -68,25 +85,25 @@ public class Drive2903 extends Subsystem {
 		driveReset();
 	}
 
-	  /**
-	   * Converts inches to number of encoder counts.
-	   *
-	   * @param inches	The value in inches to convert
-	   */
-	public double convertInchesToEncoderCount(int inches)
-	{
-		return inches * COUNTS_PER_INCH;
-	}
+	  /** // ANDREW
+	   * Converts inches to number of encoder counts. // ANDREW
+	   * // ANDREW
+	   * @param inches	The value in inches to convert // ANDREW
+	   */ // ANDREW
+	public double convertInchesToEncoderCount(int inches) // ANDREW
+	{ // ANDREW
+		return inches * COUNTS_PER_INCH; // ANDREW
+	} // ANDREW
 	
-	  /**
-	   * Converts centimeters to number of encoder counts.
-	   *
-	   * @param centimeters	The value in centimeters to convert
-	   */
-	public double convertCentimetersToEncoder(int centimeters)
-	{
-		return centimeters * COUNTS_PER_CM;
-	}
+	  /** // ANDREW
+	   * Converts centimeters to number of encoder counts. // ANDREW
+	   * // ANDREW
+	   * @param centimeters	The value in centimeters to convert // ANDREW
+	   */ // ANDREW
+	public double convertCentimetersToEncoder(int centimeters) // ANDREW
+	{ // ANDREW
+		return centimeters * COUNTS_PER_CM; // ANDREW
+	} // ANDREW
 	
 	
 	public double getDistanceTraveled() {
@@ -121,8 +138,8 @@ public class Drive2903 extends Subsystem {
 	   * @param rightSpeed    The value of the right stick.
 	   */
 	public void tankDrive(double leftSpeed, double rightSpeed) {
-//		SmartDashboard.putNumber("Left Encoder", leftGetRawCount());
-//		SmartDashboard.putNumber("Right Encoder", rightGetRawCount());
+//		SmartDashboard.putNumber("Left Encoder", leftGetRawCount()); // ANDREW
+//		SmartDashboard.putNumber("Right Encoder", rightGetRawCount()); // ANDREW
 		
 		robotDrive.tankDrive(leftSpeed, rightSpeed);
 	}
@@ -167,80 +184,80 @@ public class Drive2903 extends Subsystem {
 	public void setAutoPositionMode(boolean bothSides)
 	{
 		
-		autoPositionBothSides = bothSides;
+		autoPositionBothSides = bothSides; // ANDREW
 		
-		// set the right side primary to position and the secondary to follower
-		Robot.driveSubsystem.rightFrontMotor.changeControlMode(TalonControlMode.Position);
-		Robot.driveSubsystem.rightRearMotor.changeControlMode(TalonControlMode.Follower);
+		// set the right side primary to position and the secondary to follower // ANDREW
+		Robot.driveSubsystem.rightFrontMotor.changeControlMode(TalonControlMode.Position); // ANDREW
+		Robot.driveSubsystem.rightRearMotor.changeControlMode(TalonControlMode.Follower); // ANDREW
 		
-		// talon position set up 
-		int absolutePosition = rightFrontMotor.getPulseWidthPosition() & 0xFFF;
-		rightFrontMotor.setEncPosition(absolutePosition);		
+		// talon position set up  // ANDREW
+		int absolutePosition = rightFrontMotor.getPulseWidthPosition() & 0xFFF; // ANDREW
+		rightFrontMotor.setEncPosition(absolutePosition);		 // ANDREW
 		
 		
-		// have the motors follow rightFrontMotor
-		rightFrontMotor.set(0);
-		rightRearMotor.set(rightFrontMotor.getDeviceID());
+		// have the motors follow rightFrontMotor // ANDREW
+		rightFrontMotor.set(0); // ANDREW
+		rightRearMotor.set(rightFrontMotor.getDeviceID()); // ANDREW
 
-		// Enable PID control on the talon
-		rightFrontMotor.enableControl(); 		
+		// Enable PID control on the talon // ANDREW
+		rightFrontMotor.enableControl(); 		 // ANDREW
 		
-		//Reset the encoder to zero as its current position
-		rightFrontMotor.setPosition(0);
-		rightFrontMotor.setEncPosition(0);
+		//Reset the encoder to zero as its current position // ANDREW
+		rightFrontMotor.setPosition(0); // ANDREW
+		rightFrontMotor.setEncPosition(0); // ANDREW
 
-		/* set closed loop gains in slot0 */
-		rightFrontMotor.setAllowableClosedLoopErr(0);
-		rightFrontMotor.setProfile(0);
-//		rightFrontMotor.setF(FULL_FORWARD / NATIVE_UNITS_PER_TVE);  // this needs to be FULL-FOWARD / NATIVE UNITS)
-		rightFrontMotor.setF(0);  // this needs to be FULL-FOWARD / NATIVE UNITS)
-		rightFrontMotor.setP(0.1);
-		rightFrontMotor.setI(0);
-		rightFrontMotor.setD(0);
-//		rightFrontMotor.setMotionMagicCruiseVelocity(0); 
-//		rightFrontMotor.setMotionMagicAcceleration(0);
+		/* set closed loop gains in slot0 */ // ANDREW
+		rightFrontMotor.setAllowableClosedLoopErr(0); // ANDREW
+		rightFrontMotor.setProfile(0); // ANDREW
+//		rightFrontMotor.setF(FULL_FORWARD / NATIVE_UNITS_PER_TVE);  // this needs to be FULL-FOWARD / NATIVE UNITS) // ANDREW
+		rightFrontMotor.setF(0);  // this needs to be FULL-FOWARD / NATIVE UNITS) // ANDREW
+		rightFrontMotor.setP(0.1); // ANDREW
+		rightFrontMotor.setI(0); // ANDREW
+		rightFrontMotor.setD(0); // ANDREW
+//		rightFrontMotor.setMotionMagicCruiseVelocity(0);  // ANDREW
+//		rightFrontMotor.setMotionMagicAcceleration(0); // ANDREW
 
-		// both sides are going to be monitoring position
-		if (bothSides) {
-			// put left primary into position mode and the secondary to follower
-			Robot.driveSubsystem.leftFrontMotor.changeControlMode(TalonControlMode.Position);
-			Robot.driveSubsystem.leftRearMotor.changeControlMode(TalonControlMode.Follower);	
+		// both sides are going to be monitoring position // ANDREW
+		if (bothSides) { // ANDREW
+			// put left primary into position mode and the secondary to follower // ANDREW
+			Robot.driveSubsystem.leftFrontMotor.changeControlMode(TalonControlMode.Position); // ANDREW
+			Robot.driveSubsystem.leftRearMotor.changeControlMode(TalonControlMode.Follower); // ANDREW	
 			
-			// talon position set up 
-			absolutePosition = leftFrontMotor.getPulseWidthPosition() & 0xFFF;
-			leftFrontMotor.setEncPosition(absolutePosition);
+			// talon position set up  // ANDREW
+			absolutePosition = leftFrontMotor.getPulseWidthPosition() & 0xFFF; // ANDREW
+			leftFrontMotor.setEncPosition(absolutePosition); // ANDREW
 		
-			// left side follows right front motor
-			leftFrontMotor.set(0);
-			leftRearMotor.set(leftFrontMotor.getDeviceID());
+			// left side follows right front motor // ANDREW
+			leftFrontMotor.set(0); // ANDREW
+			leftRearMotor.set(leftFrontMotor.getDeviceID()); // ANDREW
 			
-			// Enable PID control on the talon
-			leftFrontMotor.enableControl(); 		
+			// Enable PID control on the talon // ANDREW
+			leftFrontMotor.enableControl(); 		 // ANDREW
 			
-			//Reset the encoder to zero as its current position
-			leftFrontMotor.setPosition(0);
-			leftFrontMotor.setEncPosition(0);
+			//Reset the encoder to zero as its current position // ANDREW
+			leftFrontMotor.setPosition(0); // ANDREW
+			leftFrontMotor.setEncPosition(0); // ANDREW
 
-			/* set closed loop gains in slot0 */
-			leftFrontMotor.setProfile(0);
-			leftFrontMotor.setF(FULL_FORWARD / NATIVE_UNITS_PER_TVE);  // this needs to be FULL-FOWARD / NATIVE UNITS)
-			leftFrontMotor.setP(0);
-			leftFrontMotor.setI(0);
-			leftFrontMotor.setD(0);
-			leftFrontMotor.setMotionMagicCruiseVelocity(0); 
-			leftFrontMotor.setMotionMagicAcceleration(0);
+			/* set closed loop gains in slot0 */ // ANDREW
+			leftFrontMotor.setProfile(0); // ANDREW
+			leftFrontMotor.setF(FULL_FORWARD / NATIVE_UNITS_PER_TVE);  // this needs to be FULL-FOWARD / NATIVE UNITS) // ANDREW
+			leftFrontMotor.setP(0); // ANDREW
+			leftFrontMotor.setI(0); // ANDREW
+			leftFrontMotor.setD(0); // ANDREW
+			leftFrontMotor.setMotionMagicCruiseVelocity(0);  // ANDREW
+			leftFrontMotor.setMotionMagicAcceleration(0); // ANDREW
 		} 
 		
 		// only the right side is monitoring position, left side follows right side
-		else {
-			// put left side into follower mode
-			Robot.driveSubsystem.leftFrontMotor.changeControlMode(TalonControlMode.Follower);
-			Robot.driveSubsystem.leftRearMotor.changeControlMode(TalonControlMode.Follower);	
+		else { // ANDREW
+			// put left side into follower mode // ANDREW
+			Robot.driveSubsystem.leftFrontMotor.changeControlMode(TalonControlMode.Follower); // ANDREW
+			Robot.driveSubsystem.leftRearMotor.changeControlMode(TalonControlMode.Follower); // ANDREW	
 			
-			// left side follows right front motor
-			leftFrontMotor.set(rightFrontMotor.getDeviceID());
-			leftRearMotor.set(rightFrontMotor.getDeviceID());
-		}
+			// left side follows right front motor // ANDREW
+			leftFrontMotor.set(rightFrontMotor.getDeviceID()); // ANDREW
+			leftRearMotor.set(rightFrontMotor.getDeviceID()); // ANDREW
+		} // ANDREW
 	}
 	
 	public void setTeleopMode() {
@@ -249,31 +266,33 @@ public class Drive2903 extends Subsystem {
 		Robot.driveSubsystem.rightRearMotor.changeControlMode(TalonControlMode.PercentVbus);
 		Robot.driveSubsystem.leftRearMotor.changeControlMode(TalonControlMode.PercentVbus);
 		
-//		rightFrontMotor.setInverted(false);
-//		rightRearMotor.setInverted(false);
-//		leftFrontMotor.setInverted(false);
-//		leftRearMotor.setInverted(false);
+//		rightFrontMotor.setInverted(false); // ANDREW
+//		rightRearMotor.setInverted(false); // ANDREW
+//		leftFrontMotor.setInverted(false); // ANDREW
+//		leftRearMotor.setInverted(false); // ANDREW
 		
 		leftRearMotor.set(0);
 		rightRearMotor.set(0);
 		leftFrontMotor.set(0);
 		rightFrontMotor.set(0);
 	}
+	
+	
 	public void setPosition(long distanceToDrive) {
-		// TODO Auto-generated method stub
-		rightFrontMotor.changeControlMode(TalonControlMode.Position);
-		rightFrontMotor.set(distanceToDrive);
-		if (autoPositionBothSides) {
-			leftFrontMotor.changeControlMode(TalonControlMode.Position);
-			leftFrontMotor.set(distanceToDrive);
-		}
+		// TODO Auto-generated method stub // ANDREW
+		rightFrontMotor.changeControlMode(TalonControlMode.Position); // ANDREW
+		rightFrontMotor.set(distanceToDrive); // ANDREW
+		if (autoPositionBothSides) { // ANDREW
+			leftFrontMotor.changeControlMode(TalonControlMode.Position); // ANDREW
+			leftFrontMotor.set(distanceToDrive); // ANDREW
+		} // ANDREW
 	}
 
 	public void setVelocity(double velocity) {
-		rightFrontMotor.changeControlMode(TalonControlMode.PercentVbus);
-		rightFrontMotor.set(velocity);
-		leftFrontMotor.changeControlMode(TalonControlMode.PercentVbus);
-		leftFrontMotor.set(velocity);
+		rightFrontMotor.changeControlMode(TalonControlMode.PercentVbus); // ANDREW
+		rightFrontMotor.set(velocity); // ANDREW
+		leftFrontMotor.changeControlMode(TalonControlMode.PercentVbus); // ANDREW
+		leftFrontMotor.set(velocity); // ANDREW
 	}
 
 	  //Low to High gear
@@ -293,27 +312,29 @@ public class Drive2903 extends Subsystem {
 			// configure the encoders
 			rightFrontMotor.setFeedbackDevice(FeedbackDevice.QuadEncoder);
 			rightFrontMotor.reverseSensor(true);		
+			
+			 // ANDREW-TODO this should probably be uncommented and set to 256 for the grayhill encoder.  
 //			rightFrontMotor.configEncoderCodesPerRev(COUNTS_PER_MOTOR_REV);
-			leftFrontMotor.setFeedbackDevice(FeedbackDevice.QuadEncoder);
-			leftFrontMotor.reverseSensor(false);  		
-//			leftFrontMotor.configEncoderCodesPerRev(COUNTS_PER_MOTOR_REV);
-			//SmartDashboard.putNumber("Feedback Status", CANTalon.FeedbackDeviceStatus.FeedbackStatusPresent.value);
+			leftFrontMotor.setFeedbackDevice(FeedbackDevice.QuadEncoder);  // ANDREW
+			leftFrontMotor.reverseSensor(false);  		 // ANDREW
+//			leftFrontMotor.configEncoderCodesPerRev(COUNTS_PER_MOTOR_REV); // ANDREW
+			//SmartDashboard.putNumber("Feedback Status", CANTalon.FeedbackDeviceStatus.FeedbackStatusPresent.value); // ANDREW
 			
 			// configure the output
-			rightFrontMotor.configNominalOutputVoltage(+0f, -0f);
-			rightFrontMotor.configPeakOutputVoltage(+12f, -12f);
+			rightFrontMotor.configNominalOutputVoltage(+0f, -0f); // ANDREW
+			rightFrontMotor.configPeakOutputVoltage(+12f, -12f); // ANDREW
 			
-			leftFrontMotor.configNominalOutputVoltage(+0f, -0f);
-			leftFrontMotor.configPeakOutputVoltage(+12f, -12f);
+			leftFrontMotor.configNominalOutputVoltage(+0f, -0f); // ANDREW
+			leftFrontMotor.configPeakOutputVoltage(+12f, -12f); // ANDREW
 			
 			// Initialize the raw counts
-			lastRightRawCount = 0;
-			lastLeftRawCount = 0;
+			lastRightRawCount = 0; // ANDREW
+			lastLeftRawCount = 0; // ANDREW
 		  
 		  rightFrontMotor.setPosition(0);
-		  leftFrontMotor.setPosition(0);
+		  leftFrontMotor.setPosition(0); // ANDREW
 		  rightFrontMotor.setEncPosition(0);
-		  leftFrontMotor.setEncPosition(0);
+		  leftFrontMotor.setEncPosition(0); // ANDREW
 	  }
 	  
 		public int rightGetCount() {
